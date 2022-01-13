db7d60c98da1cd0e5c2862fbf47fad6b51b70315
BehrangHaghdoust <62517402+BehrangHaghdoust@users.noreply.github.com>
2022-01-13 15:03:51 +0330

    BlogCard Component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,26 @@
   <div class="home">
     <blogPost :post="welcomeScreen"/>
     <blogPost :post="post" v-for="(post, index) in sampleBlogPost" :key="index"/>
+
+    <div class="blog-card-wrap">
+      <div class="container">
+        <h3>View More Recent Blogs</h3>
+        <div class="blog-cards">
+          <blogCard :post="post" v-for="(post , index) in sampleBlogCards " :key="index"/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import blogPost from '../components/BlogPost.vue'
+import blogCard from '..//components/BlogCard.vue'
 export default {
   name: "Home",
   components: {
-    blogPost
+    blogPost,
+    blogCard
   },
   data() {
     return {
@@ -34,7 +45,24 @@ export default {
           blogCoverPhoto: "designed-for-everyone",
         },
       ],
+      sampleBlogCards:[
+        {blogTitle:"Blog Card #1" , blogCoverPhoto:"stock-1" , blogDate:"May 1, 2021"},
+        {blogTitle:"Blog Card #2" , blogCoverPhoto:"stock-2" , blogDate:"May 1, 2021"},
+        {blogTitle:"Blog Card #3" , blogCoverPhoto:"stock-3" , blogDate:"May 1, 2021"},
+        {blogTitle:"Blog Card #4" , blogCoverPhoto:"stock-4" , blogDate:"May 1, 2021"},
+      ]
     };
   },
 };
 </script>
+
+<style lang="scss" scoped>
+
+.blog-card-wrap{
+  h3{
+    font-weight: 300;
+    font-size: 28px;
+    margin-bottom: 32px;
+  }
+}
+</style>
