9e2a7645347ea0f3728441c352a489fbc98592d9
BehrangHaghdoust <62517402+BehrangHaghdoust@users.noreply.github.com>
2022-01-13 17:41:07 +0330

    Register section in Home Page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,47 @@
 <template>
   <div class="home">
-    <blogPost :post="welcomeScreen"/>
-    <blogPost :post="post" v-for="(post, index) in sampleBlogPost" :key="index"/>
+    <blogPost :post="welcomeScreen" />
+    <blogPost
+      :post="post"
+      v-for="(post, index) in sampleBlogPost"
+      :key="index"
+    />
 
     <div class="blog-card-wrap">
       <div class="container">
         <h3>View More Recent Blogs</h3>
         <div class="blog-cards">
-          <blogCard :post="post" v-for="(post , index) in sampleBlogCards " :key="index"/>
+          <blogCard
+            :post="post"
+            v-for="(post, index) in sampleBlogCards"
+            :key="index"
+          />
         </div>
+      </div>
+    </div>
+
+    <div class="updates">
+      <div class="container">
+        <h2>never miss a post. Register for your free acount today!</h2>
+        <router-link class="router-button" to="#">
+          Register for FireBlogs <Arrow class="arrow arrow-light" />
+        </router-link>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import blogPost from '../components/BlogPost.vue'
-import blogCard from '..//components/BlogCard.vue'
+import blogPost from "../components/BlogPost.vue";
+import blogCard from "..//components/BlogCard.vue";
+import Arrow from "../assets/Icons/arrow-right-light.svg";
+
 export default {
   name: "Home",
   components: {
     blogPost,
-    blogCard
+    blogCard,
+    Arrow,
   },
   data() {
     return {
@@ -45,24 +65,72 @@ export default {
           blogCoverPhoto: "designed-for-everyone",
         },
       ],
-      sampleBlogCards:[
-        {blogTitle:"Blog Card #1" , blogCoverPhoto:"stock-1" , blogDate:"May 1, 2021"},
-        {blogTitle:"Blog Card #2" , blogCoverPhoto:"stock-2" , blogDate:"May 1, 2021"},
-        {blogTitle:"Blog Card #3" , blogCoverPhoto:"stock-3" , blogDate:"May 1, 2021"},
-        {blogTitle:"Blog Card #4" , blogCoverPhoto:"stock-4" , blogDate:"May 1, 2021"},
-      ]
+      sampleBlogCards: [
+        {
+          blogTitle: "Blog Card #1",
+          blogCoverPhoto: "stock-1",
+          blogDate: "May 1, 2021",
+        },
+        {
+          blogTitle: "Blog Card #2",
+          blogCoverPhoto: "stock-2",
+          blogDate: "May 1, 2021",
+        },
+        {
+          blogTitle: "Blog Card #3",
+          blogCoverPhoto: "stock-3",
+          blogDate: "May 1, 2021",
+        },
+        {
+          blogTitle: "Blog Card #4",
+          blogCoverPhoto: "stock-4",
+          blogDate: "May 1, 2021",
+        },
+      ],
     };
   },
 };
 </script>
 
 <style lang="scss" scoped>
-
-.blog-card-wrap{
-  h3{
+.blog-card-wrap {
+  h3 {
     font-weight: 300;
     font-size: 28px;
     margin-bottom: 32px;
+  }
+}
+.updates {
+  .container {
+    padding: 100px 25px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    @media (min-width: 800px) {
+      padding: 125px 25px;
+      flex-direction: row;
+    }
+    .router-button {
+      display: flex;
+      font-size: 14px;
+      text-decoration: none;
+      @media (min-width: 800px) {
+        margin-left: auto;
+      }
+    }
+    h2 {
+      font-weight: 300;
+      font-size: 32px;
+      max-width: 425px;
+      width: 100%;
+      text-align: center;
+      text-transform: uppercase;
+      @media (min-width: 800px) {
+        text-align: initial;
+        font-size: 40px;
+        
+      }
+    }
   }
 }
 </style>
